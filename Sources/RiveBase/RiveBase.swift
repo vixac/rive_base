02e8016b86f213fd5a43485ac2cdeaa2fe57baa9
vic <vic@vixac.com>
2026-02-20 08:47:26 +0000

    first commit

---
+++ Sources/RiveBase/RiveBase.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+
+
+public class RiveBase {
+    public init() {
+        
+    }
+    public func str() -> String {
+        return "Rive Base"
+    }
+}
